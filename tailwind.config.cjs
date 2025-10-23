a3f6b154cef9f87cd67b476dd89711edcc77ffc3
Vic_Mini <w.victoria.lee@gmail.com>
2025-10-23 22:30:17 +0100

    changed whole bunch stuff trying to make the font change

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,19 @@
+// tailwind.config.cjs
 module.exports = {
   content: [
     "./hugo_stats.json",
     "./layouts/**/*.{html,htm}",
     "./content/**/*.{html,md}",
-    "./assets/**/*.{js,ts}",
+    "./assets/**/*.{js,ts,css}",
     "./static/**/*.{html,js}",
   ],
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      fontFamily: {
+        // So the 'font-sans' utility uses Libre Franklin
+        sans: ['Libre Franklin', 'ui-sans-serif', 'system-ui', '-apple-system', 'Segoe UI', 'Roboto', 'Helvetica Neue', 'Arial', 'sans-serif'],
+      },
+    },
+  },
   plugins: [],
 };
