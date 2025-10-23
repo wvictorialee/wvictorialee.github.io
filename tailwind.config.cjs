38d37de475a2822e85159e42732d6a133f217bc3
Vic_Mini <w.victoria.lee@gmail.com>
2025-10-23 19:09:58 +0100

    Add Tailwind/PostCSS configs and base setup

---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./hugo_stats.json",
+    "./layouts/**/*.{html,htm}",
+    "./content/**/*.{html,md}",
+    "./assets/**/*.{js,ts}",
+    "./static/**/*.{html,js}",
+  ],
+  theme: { extend: {} },
+  plugins: [],
+};
